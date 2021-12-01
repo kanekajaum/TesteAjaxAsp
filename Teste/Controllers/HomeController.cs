585cac18e3c6460fcea5b21025be6da25c4d604d
joao vitor <joaosantos.rodrigues9@gmail.com>
2021-11-30 23:46:47 -0400

    Adicionar arquivos de projeto.

---
+++ Teste/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using static Teste.Controllers.Produtos;
+
+namespace Teste.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult laboratorio()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SalvarListaHtmlViaAjax( string produtos)
+        {
+            List<Produtos> lista = JsonConvert.DeserializeObject<List<Produtos>>(produtos);
+            List<Produtos> listaAdicionar = new List<Produtos>();
+
+            foreach (Produtos itemProduto in lista)
+            {
+                listaAdicionar.Add(itemProduto);
+            }
+            return Json(listaAdicionar, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Teste/Controllers/Produtos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste.Controllers
+{
+    public class Produtos
+    {
+        public string Nome { get; set; }
+        public string Valor { get; set; }
+    }
+}
